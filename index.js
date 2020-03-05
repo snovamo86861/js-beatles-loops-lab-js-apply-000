d3c5c755d367a09f600c6bc042e16b96b7ddc286
Sandra Milena Nova <sandradeeter25@gmail.com>
2020-03-05 13:24:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ function theBeatlesPlay(players, instruments) {
     return allPlayers;
 }
 
-function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
-    var newFacts = [];
-    var i = 0;
-    while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
-        i++;
-    }
-    return newFacts;
+const facts=[
+“He was the last Beatle to learn to drive”,
+“He was never a vegetarian”,
+“He was a choir boy and boy scout”,
+“He hated the sound of his own voice”
+];
+function johnLennonFacts(factsArray){
+var i=0;
+var newArray=[];
+while (factsArray.length > i){
+newArray.push(`${factsArray[i]}` + “!!!”);
+i++;
+}
+return newArray;
 }
 
 function iLoveTheBeatles(num) {
