b5546fafb130b7382080e59c344122af952935cc
Sandra Milena Nova <sandradeeter25@gmail.com>
2020-03-05 13:15:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ function theBeatlesPlay(players, instruments) {
 }
 
 function johnLennonFacts() {
-   array//
-   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+   array//"While on tour, he enjoyed playing Monopoly!!!"
+      +  "Harmonica was the first instrument Lennon learned to play!!!"
+       ]var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
     var newFacts = [];
     var i = 0;
     while (i < facts.length) {
