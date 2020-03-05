9e951c87720464876457c84bcb456a9747b7cb1e
Sandra Milena Nova <sandradeeter25@gmail.com>
2020-03-05 13:27:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(players, instruments) {
 
 
 function johnLennonFacts(facts) {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
+    var results = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
     var newFacts = [];
     var i = 0;
     while (i < facts.length) {
