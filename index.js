5ac4c86780243e1bfef9f39516e965b2ef4ea816
Sandra Milena Nova <sandradeeter25@gmail.com>
2020-03-05 13:27:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(players, instruments) {
 }
 
 
-function johnLennonFacts() {
+function johnLennonFacts(facts) {
     var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
     var newFacts = [];
     var i = 0;
